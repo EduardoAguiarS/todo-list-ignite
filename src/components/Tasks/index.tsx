c1dd69bdd7633b42d7836315c22bfc7c487e04f3
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-06-29 12:07:59 -0300

    have tasks logic

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -8,19 +8,26 @@ import { Task } from "../Task";
 // Phosphor Icons
 import { ClipboardText } from "phosphor-react";
 
+// React
+import { useState } from "react";
+
 export function Tasks() {
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div className={styles.tasks__container}>
       <TasksHeader />
       <main className={styles.tasks__list}>
-        {/* <div className={styles.no__tasks}>
-          <ClipboardText size={56} />
-          <strong>Você ainda não tem tarefas cadastradas</strong>
-          <span>Crie tarefas e organize seus itens a fazer</span>
-        </div> */}
-        <Task />
-        <Task taskID="dfgsdgsdgs" />
-        <Task taskID="dfgsdgsdgsds" />
+        {tasks.length === 0 && (
+          <div className={styles.no__tasks}>
+            <ClipboardText size={56} />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div>
+        )}
+        {tasks.map(task => {
+          return <Task />;
+        })}
       </main>
     </div>
   );
